749af52f5713f84a131aa8e5550548e4fe42474f
M3ntru <zatd39@gmail.com>
2022-04-13 15:07:49 +0800

    version 0.1.0
    - 初版

--- src/App.js
+++ src/App.js
@@ -1,23 +1,314 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css";
+import Cheer from "./cheer.json";
+import copy from "copy-to-clipboard";
+import Converter from "./Converter";
+import parse from "html-react-parser";
 
 function App() {
+  const [number, setNumber] = useState("0");
+  const [result, setResult] = useState("");
+  const [type, setType] = useState("");
+  const [alert, setAlert] = useState(false);
+  const [numberAlert, setNumberAlert] = useState(false);
+  // const [random, setRandom] = useState(0);
+  // const type = ["1", "100", "1000", "5000", "10000"];
+
+  // useEffect(() => {
+  //   let rotationInterval = setInterval(() => {
+  //     setRandom((random) => (random + 1) % 5);
+  //     console.log("AA");
+  //   }, 1000);
+  //   return () => clearInterval(rotationInterval);
+  // }, [  ]);
+
+  const clickCheer = (value) => {
+    setType(value);
+    if (parseInt(number) >= 1000) {
+      setAlert(true);
+    } else {
+      addCheer(value);
+    }
+  };
+
+  const addCheer = (value) => {
+    setAlert(false);
+    const amount = number && number !== "0" ? number : "87";
+    const bitString =
+      result === "" || result.slice(result.length - 1) === " "
+        ? `${value + amount} `
+        : ` ${value + amount} `;
+    console.log(bitString);
+    setResult((result) => result + bitString);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="flex flex-wrap bg-gray-400 p-5 mx-10 my-3 rounded shadow max-w-5xl">
+          <div className="w-full bg-gray-400 p-5 mx-auto">
+            <div className="text-lg font-semibold text-gray-900 m-1">
+              Bits (defalut:87)
+            </div>
+            <input
+              value={number}
+              className="text-black px-2"
+              onChange={(event) => {
+                setNumber(event.target.value);
+              }}
+              type="number"
+            />
+          </div>
+          <div className="justify-center">
+            {Object.keys(Cheer).map((key, i) => (
+              <button
+                className="
+                     transition-colors
+                     flex-inline
+                     w-10
+                     h-10
+                     shadow-sm
+                     rounded
+                     p-1
+                     m-1.5
+                     justify-center
+                     items-center
+                     text-md
+                     bg-white text-gray-700
+                   "
+                key={key}
+                title={key}
+                onClick={() => clickCheer(key)}
+              >
+                <img src={Cheer[key][100]} alt="logo" />
+              </button>
+            ))}
+          </div>
+          <div className="flex flex-wrap bg-gray-400 w-full p-5 my-3 mx-4 md:mx-12 lg:mx-16">
+            <div className="text-lg font-semibold text-gray-900 m-1">
+              Preview
+            </div>
+            <span
+              className="
+              preview
+              break-all
+              form-control
+              text-left
+              inline-block
+              w-full
+              px-3
+              py-1.5
+              m-2
+              h-auto
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-gray-600 focus:outline-none
+            "
+              style={{minHeight: "8em"}}
+            >
+              {parse(
+                Converter.formatText(
+                  result,
+                  [".", "!", "?", ":", ";", ",", " "],
+                  90,
+                  null
+                ).display
+              )}
+            </span>
+            <div
+              className={`text-lg font-semibold ${
+                result.length < 500 ? "text-gray-900" : "text-red-600"
+              } m-1`}
+            >
+              Input & Result ( {result.length} / 500 )
+            </div>
+            <textarea
+              className="
+              break-all
+              form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              m-2
+              h-32
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-gray-600 focus:outline-none
+            "
+              value={result}
+              onChange={(event) => {
+                setResult(event.target.value);
+              }}
+            ></textarea>
+            <div
+              className="
+              flex
+              items-center
+              p-2
+              mx-auto
+            "
+            >
+              <button
+                className="
+                w-full
+                bg-white
+                hover:bg-gray-200
+                text-gray-700
+                text-lg
+                font-bold
+                py-1
+                px-2
+                mt-2
+                rounded
+                border border-gray-800
+              "
+                type="button"
+                onClick={() => {
+                  copy(result);
+                }}
+              >
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
       </header>
+      <div
+        className={`
+          ${alert ? "flex" : "hidden"}
+          bg-gray-700 bg-opacity-75
+          overflow-y-auto overflow-x-hidden
+          fixed
+          right-0
+          left-0
+          top-0
+          z-50
+          justify-center
+          items-center
+          inset-0
+          h-modal`}
+      >
+        <div className="relative px-4">
+          <div
+            className="
+              h-auto
+              w-auto
+              relative
+              bg-white
+              rounded-lg
+              shadow-2xl
+              border-6 border-gray-600
+
+            "
+          >
+            <div
+              className="
+                justify-between
+                items-center
+                pt-5
+                rounded-t
+                text-center
+              "
+            >
+              <h3 className="text-3xl font-bold text-gray-900">冷靜</h3>
+            </div>
+            <table className="min-w-full divide-y divide-gray-200">
+              <tbody className="bg-white divide-y divide-gray-200">
+                <tr>
+                  <td className="px-6 py-4">
+                    <div
+                      className="
+                        text-lg text-gray-900
+                        font-bold 
+                        flex flex-wrap
+                        place-content-center
+                      "
+                    >
+                      <div className="preview w-full">
+                        {parse(
+                          Converter.formatText(
+                            type + number,
+                            [".", "!", "?", ":", ";", ",", " "],
+                            90,
+                            null
+                          ).display
+                        )}
+                      </div>
+                      <div className="flex flex-wrap my-1">
+                        這是娛樂消費，請遵循量力而為原則
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div
+              className="
+                items-center
+                p-6
+                space-x-2
+                rounded-b
+                border-t border-gray-200
+                grid grid-cols-2
+              "
+            >
+              <button
+                className="
+                  w-full
+                  bg-transparent
+                  text-gray-800
+                  hover:text-gray-400
+                  font-bold
+                  py-2
+                  px-4
+                  border border-gray-800
+                  hover:border-gray-400
+                  rounded
+                  col-span-1
+                "
+                type="button"
+                onClick={() => {
+                  setAlert(false);
+                }}
+              >
+                想一下
+              </button>
+              <button
+                className="
+                  w-full
+                  bg-green-700
+                  hover:bg-green-500
+                  text-white
+                  font-bold
+                  py-2
+                  px-4
+                  rounded
+                  col-span-1
+                "
+                type="button"
+                onClick={() => addCheer(type)}
+              >
+                塞啦
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Converter.js
@@ -0,0 +1,122 @@
+import cheerJson from './cheer.json'
+class Converter {
+    splitTextV1 = (t, e, n, c, v) => {
+        var r = [];
+        function text(o, i) {
+            if (e[i] && o.trim().length)
+                if (o.indexOf(e[i]) > -1) {
+                    var a = 1
+                        , s = o.split(e[i]);
+                    s.forEach((function (o) {
+                        if (o.length) {
+                            var c = "";
+                            a !== s.length && (c = e[i]);
+                            o = (o + c).trim()
+                        }
+                        o.length && o.length <= n ? r.push(o) : text(o, i + 1);
+                        a++
+                    }
+                    ))
+                } else
+                    text(o, i + 1);
+            else if (o.length)
+                for (var c = new RegExp(".{1,".concat(n, "}"), "g"), u = o.match(c); u.length > 0;)
+                    r.push(u.shift().trim())
+        };
+        text(t, 0);
+
+        var out = [];
+        r.forEach(function (t) {
+            out.length && out[out.length - 1].length + t.length + 1 <= n ? out[out.length - 1] += " ".concat(t) : out.push(t)
+        })
+        var cheerRegex = /([0-9])\d+/g;
+        var cheerList = c.match(cheerRegex);
+        var cheerTotal = 0;
+        var doodle = false;
+        if (cheerList) {
+            cheerList.forEach(function (t) {
+                cheerTotal = cheerTotal + parseInt(t);
+                if (parseInt(t) >= 1e5) {
+                    doodle = true;
+                }
+            })
+        }
+        var data = {
+            count: cheerTotal,
+            display: v,
+            message: out,
+            doodle
+        }
+        return data;
+    }
+
+    formatText = (t, e, n, z) => {
+        var space = /\s+/g;
+        //var comma = /,/g;
+        var vv = t;
+        t = t.replace(space, ' ');
+        //t = t.replace(comma, '');
+        var u = vv,
+            l = t,
+            f = ["cheer", "doodlecheer", "biblethump", "cheerwhal", "corgo", "scoops", "party", "seemsgood", "pride", "uni", "showlove", "kappa", "frankerz", "heyguys", "dansgame", "elegiggle", "trihard", "kreygasm", "4head", "swiftrage", "notlikethis", "failfish", "vohiyo", "pjsalt", "mrdestructoid", "bday", "ripcheer", "shamrock", "bitboss", "streamlabs", "muxy", "holidaycheer", "goal", "anon", "charity", "tgm3cheer"]
+        u.length > 500 && (u = "".concat(u.substring(0, 296), "..."));
+        l.length > 550 && (l = "".concat(l.substring(0, 446), "..."));
+        var d = new RegExp("\\b(".concat(f.join("|"), ")([0-9]+)\\b"), "gi"),
+            m = /(^|\s)([a-zA-Z0-9][a-zA-Z]+|[a-zA-Z0-9]+cheer)([0-9]+)(\s|$)/i,
+            y = u.match(d),
+            g = [];
+        y && (g = y.map((function (t) {
+            return m.exec(t)
+        })));
+        var c = "";
+        var b = l;
+        var v = u;
+        v = this.formatTwitchEmotes(v, z);
+        g.forEach((function (t) {
+            var color, height;
+            var temp1 = t[2].toLowerCase();
+            var i = parseInt(t[3]);
+            if (parseInt(t[3]) >= 1e5) {
+                if (temp1 === 'cheer' || temp1 === 'doodlecheer') { color = "#f3a71a"; height = 100000; }
+                else { color = "red"; height = 10000; }
+            }
+            if (parseInt(t[3]) >= 1e4 && parseInt(t[3]) <= 99999) { color = "red"; height = 10000; }
+            if (parseInt(t[3]) >= 5e3 && parseInt(t[3]) <= 9999) { color = "blue"; height = 5000; }
+            if (parseInt(t[3]) >= 1e3 && parseInt(t[3]) <= 4999) { color = "#2dfdbe"; height = 1000; }
+            if (parseInt(t[3]) >= 100 && parseInt(t[3]) <= 999) { color = "#be61ff"; height = 100; }
+            if (parseInt(t[3]) >= 1 && parseInt(t[3]) <= 99) { color = "grey"; height = 1; }
+            var temp2 = cheerJson[temp1][height];
+            v = v.replace(t[0], "<img src='" + temp2 + "' /><font class='font-bold' color='" + color + "'>" + t[3] + "</font>");
+            b = b.replace(t[0], "");
+            c = c + t[0] + ' ';
+        }))
+        l = b;
+        l = l.replace(/((?:https?|ftp):\/\/[\n\S]+)|(<([^>]+)>)+/gi, "").trim();
+        return this.splitTextV1(l, e, n, c, v);
+    }
+
+    formatTwitchEmotes = (text, emotes) => {
+        let link = 'http://static-cdn.jtvnw.net/emoticons/v2/';
+        var splitText = Array.from(text);
+        for (var i in emotes) {
+            var e = emotes[i];
+            for (var j in e) {
+                var mote = e[j];
+                if (typeof mote === 'string') {
+                    mote = mote.split('-');
+                    mote = [parseInt(mote[0]), parseInt(mote[1])];
+                    var length = mote[1] - mote[0];
+                    var empty = Array.apply(null, new Array(length + 1)).map(function () { return ''; });
+                    splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
+                    splitText.splice(mote[0], 1, `<img class="emoticon" src="${link}${i}/default/dark/3.0">`);
+                };
+            };
+        }
+        return splitText.join('');
+    }
+}
+
+export default new Converter()
+
+
+
